81cbdc09fa62c32e38bf61ae2ca5101bd6394f30
Anton Kukso <anton.kuxo@gmail.com>
2025-02-18 01:54:46 +0300

    Some fixes after Github check

--- HW_7.py
+++ HW_7.py
@@ -6,13 +6,13 @@ while not correct:
     if len(number_player) != 4:
         print("Not 4 digits, please, try again!")
         continue
-    elif not number_player.isdigit():
+    if not number_player.isdigit():
         print("Not digits, please, try again!")
         continue
-    elif len(set(number_player)) != 4:
+    if len(set(number_player)) != 4:
         print("Not unique numbers, please, try again")
         continue
-    elif number_player == number_computer:
+    if number_player == number_computer:
         print("You win")
         correct = True
     else:
@@ -20,26 +20,26 @@ while not correct:
         cows = 0
         for i in range(4):
             if number_player[i] == number_computer[i]:
-                bulls +=1
+                bulls += 1
             elif number_player[i] in number_computer:
-                cows +=1
+                cows += 1
         print(f"Bulls: {bulls}, Cows: {cows}")
 
 # 2.
 N = 10
 for i in range(N):
     for j in range(N - i - 1):
-        print(" ", end = "")
+        print(" ", end ="")
     for k in range(i * 2 + 1):
-        print("*", end = "")
+        print("*", end ="")
     print()
 
 # 3.
-l = [6, 2, 3, 8]
-l_a = []
-for i in range(2,9):
-    l_a.append(i)
-l = set(l)
-l_a = set(l_a)
-l_f = l.symmetric_difference(l_a)
-print(len(l_f))
+list_a = [6, 2, 3, 8]
+list_b = []
+for i in range(2, 9):
+    list_b.append(i)
+list_a = set(list_a)
+list_b = set(list_b)
+list_c = list_a.symmetric_difference(list_b)
+print(len(list_c))
