3ef17b3f66165e4e219459558592a1272b5a4e16
Anton Kukso <anton.kuxo@gmail.com>
2025-03-02 14:43:09 +0300

    Homework 11 is ready for review

--- HW_11.py
+++ HW_11.py
@@ -1,77 +1,77 @@
 # 1.
-# def validate_arguments(func):
-#     def wrapper(*args, **kwargs):
-#         for arg in args:
-#             if not (isinstance(arg, (int, float)) and arg > 0):
-#                 raise ValueError(f"{arg} не является положительным числом.")
-#         for key, value in kwargs.items():
-#             if not (isinstance(value, (int, float)) and value > 0):
-#                 raise ValueError(f"{key}={value} не является положительным числом.")
-#         return func(*args, **kwargs)
-#     return wrapper
-#
-# @validate_arguments
-# def my_function(a, b, c):
-#     return a + b + c
-#
-# print(my_function(7, 8, 9))
-# try:
-#     print(my_function(-7, 8, 9))
-# except ValueError as e_value:
-#     print(e_value)
+def validate_arguments(func):
+    def wrapper(*args, **kwargs):
+        for arg in args:
+            if not (isinstance(arg, (int, float)) and arg > 0):
+                raise ValueError(f"{arg} не является положительным числом.")
+        for key, value in kwargs.items():
+            if not (isinstance(value, (int, float)) and value > 0):
+                raise ValueError(f"{key}={value} не является положительным числом.")
+        return func(*args, **kwargs)
+    return wrapper
+
+@validate_arguments
+def my_function(a, b, c):
+    return a + b + c
+
+print(my_function(7, 8, 9))
+try:
+    print(my_function(-7, 8, 9))
+except ValueError as e_value:
+    print(e_value)
 
 # 2.
-# def check_numbers(func):
-#     def wrapper(*args, **kwargs):
-#         result = func(*args, **kwargs)
-#         if not isinstance(result, (int, float)):
-#             print(f"Ошибка: {result} не является числом.")
-#         return result
-#     return wrapper
-# @check_numbers
-# def adding(a, b):
-#     return a + b
-#
-# @check_numbers
-# def hello(name):
-#     return f"Привет, {name}!"
-#
-# print(adding(8, 8))
-# print(hello("Bob"))
+def check_numbers(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        if not isinstance(result, (int, float)):
+            print(f"Ошибка: {result} не является числом.")
+        return result
+    return wrapper
+@check_numbers
+def adding(a, b):
+    return a + b
+
+@check_numbers
+def hello(name):
+    return f"Привет, {name}!"
+
+print(adding(8, 8))
+print(hello("Bob"))
 
 # 3.
-# def typed(type):
-#     def decorator(func):
-#         def wrapper(*args, **kwargs):
-#             converted_args = [type(arg) for arg in args]
-#             converted_kwargs = {key: type(value) for key, value in kwargs.items()}
-#             result = func(*converted_args, **converted_kwargs)
-#
-#
-#             return result
-#         return wrapper
-#     return decorator
-#
-#
-# @typed(type = str)
-# def add(a, b):
-#     return a + b
-#
-# print(add("3", 5))
-# print(add(5, 5))
-# print(add('a', 'b'))
-#
-# @typed(type = int)
-# def add(a, b, c):
-#     return a + b + c
-#
-# print(add(5, 6, 7))
-#
-# @typed(type = float)
-# def add(a, b, c):
-#     return a + b + c
-#
-# print(add(0.1, 0.2, 0.4))
+def typed(type):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            converted_args = [type(arg) for arg in args]
+            converted_kwargs = {key: type(value) for key, value in kwargs.items()}
+            result = func(*converted_args, **converted_kwargs)
+
+
+            return result
+        return wrapper
+    return decorator
+
+
+@typed(type = str)
+def add(a, b):
+    return a + b
+
+print(add("3", 5))
+print(add(5, 5))
+print(add('a', 'b'))
+
+@typed(type = int)
+def add(a, b, c):
+    return a + b + c
+
+print(add(5, 6, 7))
+
+@typed(type = float)
+def add(a, b, c):
+    return a + b + c
+
+print(add(0.1, 0.2, 0.4))
 
 # 4.
 def cache(func):
