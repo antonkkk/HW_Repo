49c03f591f1f1a399de24d47c430580c2c314322
Anton Kukso <anton.kuxo@gmail.com>
2025-03-18 19:21:50 +0300

    First task

---
+++ test_tasks/Task_1.py
@@ -0,0 +1,16 @@
+# 1.
+text = "ПрограммированиеPython"
+first_letter = text[0]
+last_letter = text[-1]
+third_letter = text[2]
+third_l_letter = text[-3]
+text_length = len(text)
+reversed_text = text[::-1]
+first_eight_letters = text[:8]
+print(first_letter)
+print(last_letter)
+print(third_letter)
+print(third_l_letter)
+print(text_length)
+print(reversed_text)
+print(first_eight_letters)
