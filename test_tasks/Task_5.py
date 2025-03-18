8ab026efffeaa04ff4bcd192c8b228a1ce84dc15
Anton Kukso <anton.kuxo@gmail.com>
2025-03-18 20:09:26 +0300

    Fifth task

---
+++ test_tasks/Task_5.py
@@ -0,0 +1,12 @@
+# 5.
+def is_palindrome(input_value):
+    input_str = str(input_value)
+
+    return input_str == input_str[::-1]
+
+user_input = input("Введите строку или число: ")
+
+if is_palindrome(user_input):
+    print(f" {user_input} - это палиндром")
+else:
+    print(f" {user_input} - это не палиндром")
