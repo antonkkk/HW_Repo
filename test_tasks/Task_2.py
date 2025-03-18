760822b3791643c6b49d5dc62d6d11b0754b5a3c
Anton Kukso <anton.kuxo@gmail.com>
2025-03-18 19:30:44 +0300

    Second task

---
+++ test_tasks/Task_2.py
@@ -0,0 +1,17 @@
+# 2.
+def calculate_square():
+    user_input = float(input("Введите число!"))
+    value = user_input ** 2
+    print(f"Квадрат числа {user_input} равен {value}")
+
+calculate_square()
+
+def check_even_odd():
+    user_number = int(input("Введите целое число: "))
+
+    if user_number % 2 == 0:
+        print(f"Число {user_number} является четным.")
+    else:
+        print(f"Число {user_number} является нечетным.")
+
+check_even_odd()
